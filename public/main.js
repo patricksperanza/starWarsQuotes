a14d90dbffb9aa54c22b9a9537b8148179a3795d
Patrick Speranza <patrick.m.speranza@gmail.com>
2022-11-03 20:43:35 -0500

    fixed put and delete requests

--- public/main.js
+++ public/main.js
@@ -1,43 +1,41 @@
-const { del } = require("express/lib/application")
+const update = document.querySelector("#update-button");
+const deleteButton = document.querySelector("#delete-button");
+const messageDiv = document.querySelector("#message");
 
-const update = document.querySelector('#update-button')
-const deleteButton = document.querySelector('#delete-button')
-const messageDiv = document.querySelector('#message')
-
-update.addEventListener('click', _ => {
-  fetch('/quotes', {
-    method: 'put',
-    headers: { 'Content-Type': 'application/json' },
+update.addEventListener("click", (_) => {
+  fetch("/quotes", {
+    method: "put",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      name: 'Darth Vadar',
-      quote: 'I find your lack of faith disturbing.'
-    })
+      name: "Darth Vader",
+      quote: "I find your lack of faith disturbing.",
+    }),
   })
-    .then(res => {
-      if (res.ok) return res.json()
+    .then((res) => {
+      if (res.ok) return res.json();
     })
-    .then(response => {
-      window.location.reload(true)
-    })
-})
+    .then((response) => {
+      window.location.reload(true);
+    });
+});
 
-deleteButton.addEventListener('click', _ => {
-  fetch('/quotes', {
-    method: 'delete',
-    headers: { 'Content-Type': 'application/json' },
+deleteButton.addEventListener("click", (_) => {
+  fetch("/quotes", {
+    method: "delete",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      name: 'Darth Vadar'
-    })
+      name: "Darth Vader",
+    }),
   })
-    .then(res => {
-      if (res.ok) return res.json()
+    .then((res) => {
+      if (res.ok) return res.json();
     })
-    .then(response => {
-      if (response === 'No quote to delete') {
-        messageDiv.textContent = 'No Darth Vadar quote to delete'
+    .then((response) => {
+      if (response === "No quote to delete") {
+        messageDiv.textContent = "No Darth Vader quote to delete";
       } else {
-        window.location.reload(true)
+        window.location.reload(true);
       }
     })
-    .catch(console.error)
-})
+    .catch(console.error);
+});
--- server.js
+++ server.js
@@ -1,90 +1,87 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const app = express();
 
 // ========================
 // Link to Database
 // ========================
-// Updates environment variables
-// @see https://zellwk.com/blog/environment-variables/
-// require('./dotenv')
 
-// Replace process.env.DB_URL with your actual connection string
-// const connectionString = process.env.DB_URL
-
-MongoClient.connect('mongodb+srv://patricksperanza:Pat&020191@cluster0.7e4ie0h.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true })
-  .then(client => {
-    console.log('Connected to Database')
-    const db = client.db('star-wars-quotes')
-    const quotesCollection = db.collection('quotes')
+const connectionString =
+  "mongodb+srv://patricksperanza:Pat&020191@cluster0.7e4ie0h.mongodb.net/?retryWrites=true&w=majority";
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then((client) => {
+    console.log("Connected to Database");
+    const db = client.db("star-wars-quotes");
+    const quotesCollection = db.collection("quotes");
 
     // ========================
     // Middlewares
     // ========================
-    app.set('view engine', 'ejs')
-    app.use(bodyParser.urlencoded({ extended: true }))
-    app.use(bodyParser.json())
-    app.use(express.static('public'))
+    app.set("view engine", "ejs");
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.use(express.static("public"));
 
     // ========================
     // Routes
     // ========================
-    app.get('/', (req, res) => {
-      db.collection('quotes').find().toArray()
-        .then(results => {
-          res.render('index.ejs', { quotes: results })
+    app.get("/", (req, res) => {
+      db.collection("quotes")
+        .find()
+        .toArray()
+        .then((quotes) => {
+          res.render("index.ejs", { quotes: quotes });
         })
-        .catch(error => console.error(error))
-    })
+        .catch(/* ... */);
+    });
 
-    app.post('/quotes', (req, res) => {
-        console.log(req.body)
-      quotesCollection.insertOne(req.body)
-        .then(result => {
-          res.redirect('/')
+    app.post("/quotes", (req, res) => {
+      quotesCollection
+        .insertOne(req.body)
+        .then((result) => {
+          res.redirect("/");
         })
-        .catch(error => console.error(error))
-    })
+        .catch((error) => console.error(error));
+    });
 
-    app.put('/quotes', (req, res) => {
-        console.log(req.body)
-      quotesCollection.findOneAndUpdate(
-        { name: 'Yoda' },
-        {
-          $set: {
-            name: req.body.name,
-            quote: req.body.quote
+    app.put("/quotes", (req, res) => {
+      quotesCollection
+        .findOneAndUpdate(
+          { name: "Yoda" },
+          {
+            $set: {
+              name: req.body.name,
+              quote: req.body.quote,
+            },
+          },
+          {
+            upsert: true,
           }
-        },
-        {
-          upsert: true
-        }
-      )
-        .then(result => res.json('Success'))
-        .catch(error => console.error(error))
-    })
+        )
+        .then((result) => res.json("Success"))
+        .catch((error) => console.error(error));
+    });
 
-    app.delete('/quotes', (req, res) => {
-      quotesCollection.deleteOne(
-        { name: req.body.name }
-      )
-        .then(result => {
+    app.delete("/quotes", (req, res) => {
+      quotesCollection
+        .deleteOne({ name: req.body.name })
+        .then((result) => {
           if (result.deletedCount === 0) {
-            return res.json('No quote to delete')
+            return res.json("No quote to delete");
           }
-          res.json('Deleted Darth Vadar\'s quote')
+          res.json("Deleted Darth Vader's quote");
         })
-        .catch(error => console.error(error))
-    })
+        .catch((error) => console.error(error));
+    });
 
     // ========================
     // Listen
     // ========================
-    const isProduction = process.env.NODE_ENV === 'production'
-    const port = isProduction ? 7500 : 3000
+    const isProduction = process.env.NODE_ENV === "production";
+    const port = isProduction ? 7500 : 3000;
     app.listen(port, function () {
-      console.log(`listening on ${port}`)
-    })
+      console.log(`listening on ${port}`);
+    });
   })
-  .catch(console.error)
+  .catch(console.error);
